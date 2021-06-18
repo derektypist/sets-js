5a426e539e6e3c59f6e6231747c5d91aaf49a475
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-18 12:15:38 +0000

    Updated comments at script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Set Up Variable
 let txt = "";
 
-// Create Instances of Set
+// Create Instances of the Set Class
 let metals = new Set(["Lithium","Sodium","Magnesium","Aluminium"]);
 let numbers = new Set([1,2,3,4,5,6]);
 
@@ -9,11 +9,11 @@ let numbers = new Set([1,2,3,4,5,6]);
 let metalsArr = Array.from(metals);
 let numbersArr = Array.from(numbers);
 
-// View Original Metals and Numbers
-txt += `Original Metals ${metalsArr} <br>`;
-txt += `Original Numbers ${numbersArr} <br>`;
+// Add Original Metals and Numbers to txt
+txt += `Original Metals - ${metalsArr} <br>`;
+txt += `Original Numbers - ${numbersArr} <br>`;
 
-// Add Items to Each Set
+// Add Items to Each Set (e.g. Potassium to Metals, 7 to Numbers)
 metals.add("Potassium");
 numbers.add(7);
 
@@ -21,9 +21,11 @@ numbers.add(7);
 let updatedMetalsArr = Array.from(metals);
 let updatedNumbersArr = Array.from(numbers);
 
-// View Updated Metals and Numbers
-txt += `Updated Metals ${updatedMetalsArr} <br>`;
-txt += `Updated Numbers ${updatedNumbersArr} <br>`;
+// Add Updated Metals and Numbers to txt
+txt += `Updated Metals - ${updatedMetalsArr} <br>`;
+txt += `Updated Numbers - ${updatedNumbersArr} <br>`;
 txt += `Successfully Added Potassium to Metals <br>`;
 txt += `Successfully Added 7 to Numbers <br>`;
+
+// Display Information in the Browser Window
 document.getElementById("demo").innerHTML = txt;
