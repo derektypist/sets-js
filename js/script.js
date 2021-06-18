c960d82f852a8934031b2e208b19e0024e39175b
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-18 12:10:39 +0000

    Added add method to sets at script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,27 @@ let txt = "";
 
 // Create Instances of Set
 let metals = new Set(["Lithium","Sodium","Magnesium","Aluminium"]);
+let numbers = new Set([1,2,3,4,5,6]);
 
-// Convert Set to Array
+// Convert Sets to Arrays
 let metalsArr = Array.from(metals);
-txt += `Original Metals ${metalsArr}`;
+let numbersArr = Array.from(numbers);
+
+// View Original Metals and Numbers
+txt += `Original Metals ${metalsArr} <br>`;
+txt += `Original Numbers ${numbersArr} <br>`;
+
+// Add Items to Each Set
+metals.add("Potassium");
+numbers.add(7);
+
+// Convert Updated Sets to Arrays
+let updatedMetalsArr = Array.from(metals);
+let updatedNumbersArr = Array.from(numbers);
+
+// View Updated Metals and Numbers
+txt += `Updated Metals ${updatedMetalsArr} <br>`;
+txt += `Updated Numbers ${updatedNumbersArr} <br>`;
+txt += `Successfully Added Potassium to Metals <br>`;
+txt += `Successfully Added 7 to Numbers <br>`;
 document.getElementById("demo").innerHTML = txt;
